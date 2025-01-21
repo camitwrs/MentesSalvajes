d03dd24c38b2e3b90a63359099534298383720ec
camitwrs <milatorresv23@gmail.com>
2025-01-21 20:51:58 -0300

    avance backend de autenticacion

--- server/routes/export.js
+++ server/routes/export.js
@@ -4,6 +4,7 @@ const cuestionariosRoutes = require("./cuestionarios.routes");
 const preguntasRoutes = require("./preguntas.routes");
 const alternativasRoutes = require("./alternativas.routes");
 const respuestasRoutes = require("./respuestas.routes");
+const autenticacionRoutes = require("./autenticacion.routes");
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use("/cuestionarios", cuestionariosRoutes);
 router.use("/preguntas", preguntasRoutes);
 router.use("/alternativas", alternativasRoutes);
 router.use("/respuestas", respuestasRoutes);
+router.use("/autenticacion", autenticacionRoutes);
 
 module.exports = router;
