ba3a4248ea798fc8fd83d876e1433f6e9b495742
camitwrs <milatorresv23@gmail.com>
2025-03-20 22:32:49 -0300

    server.js modificado sin servir el front

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,7 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import dotenv from "dotenv"; // Importa dotenv para cargar variables de entorno
 import routes from "./routes/export.js"; // Asegúrate de incluir la extensión .js
-import path from "path";
 
-import { fileURLToPath } from "url"; // Necesario para __dirname en ES Modules
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
 const app = express();
@@ -32,13 +25,6 @@ app.use(cookieParser());
 
 // Rutas principales
 app.use("/api", routes);
-
-// --- Servir React build ---
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
 
 // Ruta por defecto para manejar errores de rutas no definidas
 app.use((req, res) => {
